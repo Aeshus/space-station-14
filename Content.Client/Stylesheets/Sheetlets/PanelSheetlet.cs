0ff47c3a97756fb7307d917626ec5bd7e5f12cef
Thomas (Aeshus) <git@aeshus.com>
2026-07-11 17:58:43 -0500

    Update ButtonSheetlet and users

--- Content.Client/Stylesheets/Sheetlets/PanelSheetlet.cs
+++ Content.Client/Stylesheets/Sheetlets/PanelSheetlet.cs
@@ -69,15 +69,15 @@ public sealed class PanelSheetlet<T> : ISheetlet<T>
             // panels that have the same corner bezels as buttons
             E()
                 .Class(StyleClass.BackgroundPanel)
-                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.BaseStyleBox(config))
+                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.BaseStyleBox(factory, config))
                 .Modulate(config.SecondaryPalette.Background),
             E()
                 .Class(StyleClass.BackgroundPanelDark)
-                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.BaseStyleBox(config))
+                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.BaseStyleBox(factory, config))
                 .Modulate(config.SecondaryPalette.BackgroundDark),
             E()
                 .Class(StyleClass.BackgroundPanelOpenLeft)
-                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.OpenLeftStyleBox(config))
+                .Prop(PanelContainer.StylePropertyPanel, StyleBoxHelpers.OpenLeftStyleBox(factory, config))
                 .Modulate(config.SecondaryPalette.Background),
             E()
                 .Class(StyleClass.BackgroundPanelOpenRight)
