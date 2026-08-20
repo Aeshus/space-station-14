199569ac405f9b0928cc95ace3987effc5893897
Thomas (Aeshus) <git@aeshus.com>
2026-08-20 18:25:51 -0500

    Document

--- Content.Client/StyleProto/FontFamilyPrototype.cs
+++ Content.Client/StyleProto/FontFamilyPrototype.cs
@@ -1,26 +1,46 @@
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
+using Content.Client.Stylesheets.Fonts;
 
 namespace Content.Client.StyleProto;
 
+/// <summary>
+/// Prototype representing a font family stack, which combines multiple font files into a single FontFamilyStack interface.
+/// </summary>
 [Prototype]
 public sealed partial class FontFamilyPrototype : IPrototype
 {
+    /// <inheritdoc/>
     [IdDataField]
     public string ID { get; private set; }
 
+    /// <summary>
+    /// Fonts used for <see cref="FontKind.Regular"/>
+    /// </summary>
     [DataField(required: true)]
     public ResPath[] Regular;
 
+    /// <summary>
+    /// Fonts used for <see cref="FontKind.Italic"/>
+    /// </summary>
     [DataField(required: true)]
     public ResPath[] Italic;
 
+    /// <summary>
+    /// Fonts used for <see cref="FontKind.Bold"/>
+    /// </summary>
     [DataField(required: true)]
     public ResPath[] Bold;
 
+    /// <summary>
+    /// Fonts used for <see cref="FontKind.BoldItalic"/>
+    /// </summary>
     [DataField(required: true)]
     public ResPath[] BoldItalic;
 
+    /// <summary>
+    /// Fonts used in for every font kind.
+    /// </summary>
     [DataField]
     public ResPath[] Extra;
 }
