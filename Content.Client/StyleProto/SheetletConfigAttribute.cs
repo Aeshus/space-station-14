53cef8e9a5882eb0e4d0222d018c0f31d123fd38
Thomas (Aeshus) <git@aeshus.com>
2026-08-26 20:03:01 -0400

    Remove custom sheetlet config names (idk why itd be useful tbh)

--- Content.Client/StyleProto/SheetletConfigAttribute.cs
+++ Content.Client/StyleProto/SheetletConfigAttribute.cs
@@ -7,24 +7,4 @@ namespace Content.Client.StyleProto;
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 [BaseTypeRequired(typeof(SheetletConfig))]
-public sealed class SheetletConfigAttribute : Attribute
-{
-    /// <summary>
-    /// Overriding config name.
-    /// </summary>
-    public string? Name;
-
-    /// <summary>
-    /// Marks a sheetlet config for registration with an overriding name.
-    /// </summary>
-    /// <param name="name">Overriding config name</param>
-    public SheetletConfigAttribute(string name)
-    {
-        Name = name;
-    }
-
-    /// <summary>
-    /// Marks a sheetlet config for registration.
-    /// </summary>
-    public SheetletConfigAttribute() { }
-}
+public sealed class SheetletConfigAttribute : Attribute;
