4caefaf62c67046da937d0739d69bcc8d8ace5ba
Thomas (Aeshus) <git@aeshus.com>
2026-08-29 17:50:30 -0400

    Document IStylesheetManager

--- Content.Client/StyleProto/IStylesheetManager.cs
+++ Content.Client/StyleProto/IStylesheetManager.cs
@@ -1,14 +1,35 @@
 using System.Diagnostics.CodeAnalysis;
+using Content.Client.Stylesheets;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.StyleProto;
 
+/// <summary>
+/// A stylesheet manager.
+/// </summary>
 public interface IStylesheetManager
 {
-    event Action<SheetletConfigRegistry>? OnStyleReload;
+    /// <summary>
+    /// Called on all styles being reloaded for mutations to occur.
+    /// </summary>
+    event Action<Dictionary<Type, ISheetletConfig>?> OnStyleReload;
+
+    /// <summary>
+    /// Initializes the stylesheet manager.
+    /// </summary>
     void Initialize();
+
+    /// <summary>
+    /// Reloads all current prototypes,
+    /// </summary>
     void ReloadStylesheets();
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="proto"></param>
+    /// <param name="accessor"></param>
+    /// <returns></returns>
     bool TryStyleSubscription(ProtoId<StylesheetPrototype> proto,
         [NotNullWhen(true)] out StylesheetManager.StylesheetAccessor? accessor);
 }
