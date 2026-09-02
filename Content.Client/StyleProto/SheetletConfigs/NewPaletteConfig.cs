c6c31c1d36579c267c5c66af9bd05519136ea998
Thomas (Aeshus) <git@aeshus.com>
2026-09-02 11:37:45 -0400

    Delete NewPaletteConfig
    Was a bad idea tbh

--- Content.Client/StyleProto/SheetletConfigs/NewPaletteConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Client.StyleProto.SheetletConfigs;
-
-[SheetletConfig]
-public sealed partial class NewPaletteConfig : PaletteConfig
-{
-    [DataField]
-    public Color Quantary { get; set; }
-}
