eda5c1968d6b5d1a4fb1ada2deeccac7f2965b02
Thomas (Aeshus) <git@aeshus.com>
2026-09-03 13:05:12 -0400

    Complete it

--- Content.Client/StyleProto/Sheetlets/PaletteSheetlet.cs
+++ Content.Client/StyleProto/Sheetlets/PaletteSheetlet.cs
@@ -14,8 +14,6 @@ public sealed class PaletteSheetlet : ISheetlet
     {
         var palette = configs.GetConfig<PaletteConfig>();
 
-        throw new ArgumentException("IT WORKS");
-
         return
         [
             Element<Label>().Prop(Label.StylePropertyFontColor, palette.Primary),
