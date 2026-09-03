92a870d416c2bb1eba6a02ce5d7b163830f795f2
Thomas (Aeshus) <git@aeshus.com>
2026-09-03 12:49:41 -0400

    Use serializers

--- Content.Client/StyleProto/StylesheetPrototype.cs
+++ Content.Client/StyleProto/StylesheetPrototype.cs
@@ -34,5 +34,5 @@ public sealed partial class StylesheetPrototype : IPrototype, IInheritingPrototy
     /// </summary>
     [AlwaysPushInheritance]
     [DataField(required: true)]
-    public List<string> Sheetlets;
+    public List<ISheetlet> Sheetlets;
 }
