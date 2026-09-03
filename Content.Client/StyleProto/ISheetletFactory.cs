8b1ade7d852b56554d9d32f8bccaeb73ce627dd9
Thomas (Aeshus) <git@aeshus.com>
2026-09-03 12:49:41 -0400

    Use serializers

--- Content.Client/StyleProto/ISheetletFactory.cs
+++ Content.Client/StyleProto/ISheetletFactory.cs
@@ -10,7 +10,7 @@ public interface ISheetletFactory
     bool TryGetConfigType(string name, [NotNullWhen(true)] out Type? type);
     bool TryGetSheetletType(string name, [NotNullWhen(true)] out Type? type);
     bool TryGetConfigName(Type type, [NotNullWhen(true)] out string? name);
-    public bool TryGetSheetletName(Type type, [NotNullWhen(true)] out string? name)
+    public bool TryGetSheetletName(Type type, [NotNullWhen(true)] out string? name);
 
     ISheetlet GetSheetlet(string name);
 }
