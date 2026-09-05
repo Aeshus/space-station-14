1d24047d0f2b365885050d98d1b99d20b2d495c7
Thomas (Aeshus) <git@aeshus.com>
2026-09-05 16:08:56 -0400

    Fix serializer issues

--- Content.Client/StyleProto/SheetletListSerializer.cs
+++ Content.Client/StyleProto/SheetletListSerializer.cs
@@ -1,6 +1,7 @@
 using System.Linq;
-using Robust.Shared.Physics;
+using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager;
+using Robust.Shared.Serialization.Markdown;
 using Robust.Shared.Serialization.Markdown.Sequence;
 using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
@@ -9,9 +10,31 @@ using Robust.Shared.Serialization.TypeSerializers.Interfaces;
 namespace Content.Client.StyleProto;
 
 [TypeSerializer]
-public sealed class SheetletListSerializer : BaseTypeSerializer, ITypeValidator<List<ISheetlet>, SequenceDataNode>,
-    ITypeInheritanceHandler<List<ISheetlet>, SequenceDataNode>
+public sealed class SheetletListSerializer : BaseTypeSerializer,
+    ITypeInheritanceHandler<List<ISheetlet>, SequenceDataNode>, ITypeSerializer<List<ISheetlet>, SequenceDataNode>
 {
+    public DataNode Write(ISerializationManager serializationManager,
+        List<ISheetlet> value,
+        IDependencyCollection dependencies,
+        bool alwaysWrite = false,
+        ISerializationContext? context = null)
+    {
+        var factory = dependencies.Resolve<ISheetletFactory>();
+        var sequence = new SequenceDataNode(value.Count);
+
+        foreach (var sheetlet in value)
+        {
+            if (!factory.TryGetSheetletName(sheetlet.GetType(), out var name))
+            {
+                throw new InvalidOperationException($"{value.GetType()} is not a registered sheetlet");
+            }
+
+            sequence.Add(new ValueDataNode(name));
+        }
+
+        return sequence;
+    }
+
     public ValidationNode Validate(ISerializationManager serializationManager,
         SequenceDataNode node,
         IDependencyCollection dependencies,
@@ -19,6 +42,7 @@ public sealed class SheetletListSerializer : BaseTypeSerializer, ITypeValidator<
     {
         var list = new List<ValidationNode>();
         var seen = new HashSet<string>();
+        var factory = dependencies.Resolve<ISheetletFactory>();
 
         foreach (var entry in node)
         {
@@ -34,7 +58,12 @@ public sealed class SheetletListSerializer : BaseTypeSerializer, ITypeValidator<
                 continue;
             }
 
-            list.Add(serializationManager.ValidateNode<ISheetlet>(value, context));
+            if (!factory.TryGetSheetletType(value.Value, out _))
+            {
+                return new ErrorNode(node, $"Unknown sheetlet type '{value.Value}' in prototype!");
+            }
+
+            list.Add(new ValidatedValueNode(node));
         }
 
         return new ValidatedSequenceNode(list);
@@ -55,5 +84,28 @@ public sealed class SheetletListSerializer : BaseTypeSerializer, ITypeValidator<
         }
 
         return result;
+    }
+
+    public List<ISheetlet> Read(ISerializationManager serializationManager,
+        SequenceDataNode node,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null,
+        ISerializationManager.InstantiationDelegate<List<ISheetlet>>? instanceProvider = null)
+    {
+        var lst = new List<ISheetlet>(node.Count);
+
+        var factory = dependencies.Resolve<ISheetletFactory>();
+        foreach (var n in node)
+        {
+            if (n is not ValueDataNode entry)
+            {
+                throw new InvalidOperationException($"{n} is not a value data node");
+            }
+
+            lst.Add(factory.GetSheetlet(entry.Value));
+        }
+
+        return lst;
     }
 }
