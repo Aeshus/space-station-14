df706a80861446a4aef9a5b7a25529f75911e605
Thomas (Aeshus) <git@aeshus.com>
2026-09-04 22:01:06 -0400

    Proto register ingore for stylesheet

--- Content.Server/Entry/EntryPoint.cs
+++ Content.Server/Entry/EntryPoint.cs
@@ -108,6 +108,7 @@ namespace Content.Server.Entry
             _factory.GenerateNetIds();
 
             _proto.RegisterIgnore("parallax");
+            _proto.RegisterIgnore("stylesheet");
 
             _loc.Initialize();
 
