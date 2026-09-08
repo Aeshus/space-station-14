1a19b02591c93216de57eefd805d3d80eddcd04e
Thomas (Aeshus) <git@aeshus.com>
2026-09-08 14:51:21 -0400

    Implement closest for non-system fonts

--- Content.Client/StyleProto/Fonts/FontFamilyPrototype.cs
+++ Content.Client/StyleProto/Fonts/FontFamilyPrototype.cs
@@ -1,4 +1,3 @@
-using Content.Client.Stylesheets.Fonts;
 using Robust.Client.Graphics;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
@@ -13,11 +12,35 @@ public sealed partial class FontFamilyPrototype : IPrototype
     public string ID { get; private set; }
 
     /// <summary>
-    /// The type of font this is, for accessability overrides.
+    /// The name of the font family.
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// The type of font this is, for accessibility overrides.
     /// </summary>
     [DataField(required: true)]
     public FontType FontType { get; private set; }
 
     [DataField(required: true)]
-    public Dictionary<FontSlant, Dictionary<FontWeight, ResPath[]>> Fonts = new();
+    public Dictionary<FontSlant, Dictionary<FontWeight, ResPath[]>> Variants = new();
 }
+
+/*
+
+- type: FontFamily
+  id: NotoSans
+  name: Noto Sans
+  variants:
+    Normal:
+        Regular:
+            - "/Resources/Interface/Font/..."
+            - "/Resources/Interface/Font/..."
+            - "/Resources/Interface/Font/..."
+        Bold:
+            - "/Resources/Interface/Font/..."
+            - "/Resources/Interface/Font/..."
+    Italic:
+        Bold:
+            - "/Resources/Interface/Font/..."
+*/
