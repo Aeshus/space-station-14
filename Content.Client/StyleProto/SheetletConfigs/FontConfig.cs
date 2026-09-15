cab29e1c601d115af1d4bf77c555582c04d818e4
Thomas (Aeshus) <git@aeshus.com>
2026-09-15 16:08:04 -0400

    I may be an idiot
    Turns out a serializer actually wasn't that scary, surprising enough. I
    have ptsd from sheetletlist/sheetletconfig as I needed inheritance
    nonsense, but here it just works:tm:!!!!!!
    
    YEAHA!

--- Content.Client/StyleProto/SheetletConfigs/FontConfig.cs
+++ Content.Client/StyleProto/SheetletConfigs/FontConfig.cs
@@ -2,50 +2,18 @@ using Content.Client.StyleProto.Fonts;
 
 namespace Content.Client.StyleProto.SheetletConfigs;
 
-// Why?
-//
-// Essentially, there's no way to serialize interfaces to my understanding, due to its ambitious nature.
-//
-// The problem is that here, we only actually want to (de)serialize between a singular subtype of the interface,
-// FontFamilyBundled, and we don't want to (de)serialize from a FontFamilySystem as that's brittle.
-// Thus, we can do this hack (which is "kinda" seen in ConstructionGraphNode) so that we can write it in YAML.
-
 [SheetletConfig]
 public sealed partial class FontConfig : SheetletConfig
 {
-    [DataField("base", required: true)]
-    private FontFamilyBundled _base = default!;
+    [DataField(required: true)]
+    public IFontFamily Base { get; set; } = default!;
 
-    public IFontFamily Base
-    {
-        get => field ?? _base;
-        set;
-    }
+    [DataField(required: true)]
+    public IFontFamily Monospace { get; set; } = default!;
 
-    [DataField("monospace", required: true)]
-    private FontFamilyBundled _monospace = default!;
+    [DataField(required: true)]
+    public IFontFamily Display { get; set; } = default!;
 
-    public IFontFamily Monospace
-    {
-        get => field ?? _monospace;
-        set;
-    }
-
-    [DataField("display", required: true)]
-    private FontFamilyBundled _display = default!;
-
-    public IFontFamily Display
-    {
-        get => field ?? _display;
-        set;
-    }
-
-    [DataField("decorative", required: true)]
-    private FontFamilyBundled _decorative = default!;
-
-    public IFontFamily Decorative
-    {
-        get => field ?? _decorative;
-        set;
-    }
+    [DataField(required: true)]
+    public IFontFamily Decorative { get; set; } = default!;
 }
