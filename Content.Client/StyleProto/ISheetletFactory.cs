52091cca2b10c3dd7908f69ea4b0d82bed5bbabc
Thomas (Aeshus) <git@aeshus.com>
2026-09-19 17:35:56 -0400

    Start documenting a bit

--- Content.Client/StyleProto/ISheetletFactory.cs
+++ Content.Client/StyleProto/ISheetletFactory.cs
@@ -2,10 +2,23 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Content.Client.StyleProto;
 
+/// <summary>
+/// Factory that manages the resolution and creation of sheetlets and sheetlet configs.
+/// </summary>
+/// <seealso cref="ISheetlet"/>
+/// <seealso cref="SheetletConfig"/>
 public interface ISheetletFactory
 {
+    /// <summary>
+    /// Initializes the ISheetletFactory.
+    /// </summary>
     void Initialize();
 
+    /// <summary>
+    /// Gets the sheetlet matching the
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     T GetSheetlet<T>() where T : ISheetlet;
     bool TryGetConfigType(string name, [NotNullWhen(true)] out Type? type);
     bool TryGetSheetletType(string name, [NotNullWhen(true)] out Type? type);
