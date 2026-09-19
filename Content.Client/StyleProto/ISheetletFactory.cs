cca0a4ca69d7135c551ec04d18cc90d4f8265a68
Thomas (Aeshus) <git@aeshus.com>
2026-09-19 18:10:21 -0400

    Document, remove string-based get api
    Get the type and use the type!

--- Content.Client/StyleProto/ISheetletFactory.cs
+++ Content.Client/StyleProto/ISheetletFactory.cs
@@ -15,15 +15,50 @@ public interface ISheetletFactory
     void Initialize();
 
     /// <summary>
-    /// Gets the sheetlet matching the
+    /// Gets the sheetlet matching the type.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">Sheetlet to get</typeparam>
+    /// <exception cref="ArgumentException">If the sheetlet is not found</exception>
+    /// <returns>The sheetlet</returns>
     T GetSheetlet<T>() where T : ISheetlet;
-    bool TryGetConfigType(string name, [NotNullWhen(true)] out Type? type);
-    bool TryGetSheetletType(string name, [NotNullWhen(true)] out Type? type);
-    bool TryGetConfigName(Type type, [NotNullWhen(true)] out string? name);
-    public bool TryGetSheetletName(Type type, [NotNullWhen(true)] out string? name);
 
-    ISheetlet GetSheetlet(string name);
+    /// <summary>
+    /// Gets the sheetlet matching the type.
+    /// </summary>
+    /// <param name="type">Sheetlet type to get</param>
+    /// <exception cref="ArgumentException">If the sheetlet is not found</exception>
+    /// <returns>The sheetlet</returns>
+    ISheetlet GetSheetlet(Type type);
+
+    /// <summary>
+    /// Tries to get the config type matching the name.
+    /// </summary>
+    /// <param name="name">The name of the config</param>
+    /// <param name="type">The type of the config</param>
+    /// <returns>True if the type was found, false is not</returns>
+    bool TryGetConfigType(string name, [NotNullWhen(true)] out Type? type);
+
+    /// <summary>
+    /// Tries to get the sheetlet type matching the name.
+    /// </summary>
+    /// <param name="name">The name of the sheetlet</param>
+    /// <param name="type">The type of the sheetlet</param>
+    /// <returns>True if the type was found, false is not</returns>
+    bool TryGetSheetletType(string name, [NotNullWhen(true)] out Type? type);
+
+    /// <summary>
+    /// Tries to get the config name matching the type.
+    /// </summary>
+    /// <param name="type">The type of the sheetlet</param>
+    /// <param name="name">The name of the sheetlet</param>
+    /// <returns>True if the name was found, false is not</returns>
+    bool TryGetConfigName(Type type, [NotNullWhen(true)] out string? name);
+
+    /// <summary>
+    /// Tries to get the sheetlet name matching the type.
+    /// </summary>
+    /// <param name="type">The type of the sheetlet</param>
+    /// <param name="name">The name of the sheetlet</param>
+    /// <returns>True if the name was found, false is not</returns>
+    bool TryGetSheetletName(Type type, [NotNullWhen(true)] out string? name);
 }
