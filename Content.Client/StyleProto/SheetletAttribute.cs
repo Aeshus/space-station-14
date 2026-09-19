3997787e6e76c5558e13524f5607c03cfa63272f
Thomas (Aeshus) <git@aeshus.com>
2026-09-19 18:17:48 -0400

    Document attribute

--- Content.Client/StyleProto/SheetletAttribute.cs
+++ Content.Client/StyleProto/SheetletAttribute.cs
@@ -9,12 +9,22 @@ namespace Content.Client.StyleProto;
 [BaseTypeRequired(typeof(ISheetlet))]
 public sealed class SheetletAttribute : Attribute
 {
+    /// <summary>
+    /// Name to use in (de)serialization rather than the calculated one.
+    /// </summary>
     public string? Name;
 
+    /// <summary>
+    /// Creates a sheetlet attribute with the default calculated name.
+    /// </summary>
     public SheetletAttribute()
     {
     }
 
+    /// <summary>
+    /// Creates a sheetlet attribute with a custom name.
+    /// </summary>
+    /// <param name="name">Name to use instead of calculated</param>
     public SheetletAttribute(string name)
     {
         Name = name;
