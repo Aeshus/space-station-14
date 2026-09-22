016c319d064de2ce8df1cb71980ef4953c0cf9af
Thomas (Aeshus) <git@aeshus.com>
2026-09-22 09:54:15 -0400

    Bring back some color stuff (to be deleted soon), impl PaletteConfig

---
+++ Content.Client/Stylesheets/SheetletConfigs/PaletteConfig.cs
@@ -0,0 +1,22 @@
+using Content.Client.Stylesheets.Palette;
+
+namespace Content.Client.Stylesheets.SheetletConfigs;
+
+[SheetletConfig]
+public sealed partial class PaletteConfig : SheetletConfig
+{
+    [DataField]
+    public ColorPalette PrimaryPalette { get; set; }
+
+    [DataField]
+    public ColorPalette SecondaryPalette { get; set; }
+
+    [DataField]
+    public ColorPalette PositivePalette { get; set; }
+
+    [DataField]
+    public ColorPalette NegativePalette { get; set; }
+
+    [DataField]
+    public ColorPalette HighlightPalette { get; set; }
+}
